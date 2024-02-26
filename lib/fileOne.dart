9430b1a1ec489586aaf2fc2c7e6b0b316e741f07
bhilchandresh <csbhil17@gmail.com>
2024-02-26 19:27:02 +0530

    add class

---
+++ lib/fileOne.dart
@@ -0,0 +1,13 @@
+class one extends StatefulWidget {
+  const one({super.key});
+
+  @override
+  State<one> createState() => _oneState();
+}
+
+class _oneState extends State<one> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
